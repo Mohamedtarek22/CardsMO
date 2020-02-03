f98608e759a598c31c7afae8e35700870f5023fa
Mohamedtarek22 <55949665+Mohamedtarek22@users.noreply.github.com>
2020-02-03 18:12:44 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import TheIMG from './1239309-bigthumbnail.jpg';
+
+import './App.css';
+
+class App extends Component {
+  render(){
+  return (
+    <div className="App">
+      <ul className="grid-wrapper">
+        <li className="card">
+           <div class="app-img-wrapper">
+                <img src={TheIMG} alt="" className="app-img"/>
+                
+           </div>
+           <div className="title-bar">
+             <h1 className="card-title">Tree</h1>
+           </div>
+        </li>
+        <li className="card">
+           <div class="app-img-wrapper">
+                <img src={TheIMG} alt="" className="app-img"/>
+                
+           </div>
+           <div className="title-bar">
+             <h1 className="card-title">Tree</h1>
+           </div>
+        </li>
+        <li className="card">
+           <div class="app-img-wrapper">
+                <img src={TheIMG} alt="" className="app-img"/>
+                
+           </div>
+           <div className="title-bar">
+             <h1 className="card-title">Tree</h1>
+           </div>
+        </li>
+        <li className="card">
+           <div class="app-img-wrapper">
+                <img src={TheIMG} alt="" className="app-img"/>
+                
+           </div>
+           <div className="title-bar">
+             <h1 className="card-title">Tree</h1>
+           </div>
+        </li>
+        <li className="card">
+           <div class="app-img-wrapper">
+                <img src={TheIMG} alt="" className="app-img"/>
+                
+           </div>
+           <div className="title-bar">
+             <h1 className="card-title">Tree</h1>
+           </div>
+        </li>
+        <li className="card">
+           <div class="app-img-wrapper">
+                <img src={TheIMG} alt="" className="app-img"/>
+                
+           </div>
+           <div className="title-bar">
+             <h1 className="card-title">Tree</h1>
+           </div>
+        </li>
+        <li className="card">
+           <div class="app-img-wrapper">
+                <img src={TheIMG} alt="" className="app-img"/>
+                
+           </div>
+           <div className="title-bar">
+             <h1 className="card-title">Tree</h1>
+           </div>
+        </li>
+        <li className="card">
+           <div class="app-img-wrapper">
+                <img src={TheIMG} alt="" className="app-img"/>
+                
+           </div>
+           <div className="title-bar">
+             <h1 className="card-title">Tree</h1>
+           </div>
+        </li>
+        <li className="card">
+           <div class="app-img-wrapper">
+                <img src={TheIMG} alt="" className="app-img"/>
+                
+           </div>
+           <div className="title-bar">
+             <h1 className="card-title">Tree</h1>
+           </div>
+        </li>
+        <li className="card">
+           <div class="app-img-wrapper">
+                <img src={TheIMG} alt="" className="app-img"/>
+                
+           </div>
+           <div className="title-bar">
+             <h1 className="card-title">Tree</h1>
+           </div>
+        </li>
+        <li className="card">
+           <div class="app-img-wrapper">
+                <img src={TheIMG} alt="" className="app-img"/>
+                
+           </div>
+           <div className="title-bar">
+             <h1 className="card-title">Tree</h1>
+           </div>
+        </li>
+        </ul>   
+    </div>
+  );
+  }
+}
+
+export default App;
